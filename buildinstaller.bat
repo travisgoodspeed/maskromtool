540d512d9cd4db611f78dec195b4b49287404183
Travis Goodspeed <travis@radiantmachines.com>
2023-07-20 16:19:34 -0400

    QT 6.5.2 on Windows.

--- buildinstaller.bat
+++ buildinstaller.bat
@@ -1,5 +1,5 @@
 REM Including the path.
-set PATH=C:\Qt\Tools\QtInstallerFramework\4.5\bin;C:\Qt\Tools\CMake_64\bin;C:\Qt\6.5.1\msvc2019_64\bin;%PATH%
+set PATH=C:\Qt\Tools\QtInstallerFramework\4.5\bin;C:\Qt\Tools\CMake_64\bin;C:\Qt\6.5.2\msvc2019_64\bin;C:\Qt\Tools\QtInstallerFramework\4.6\bin;%PATH%
 
 REM Copying packages.
 xcopy/y/s Release\* Deployment\packages\com.maskromtool.maskromtool\data\
@@ -7,4 +7,5 @@ xcopy/y/s Release\* Deployment\packages\com.maskromtool.maskromtool\data\
 cd Deployment
 echo Building the installer executable.
 binarycreator.exe -c config\config.xml -p packages -f MaskRomToolInstaller
+move MaskRomToolInstaller.exe maskromtool-win-x86_64.exe
 cd ..
