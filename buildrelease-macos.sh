b974472e219aa70cc8409134b3f2a1ae98a0be29
Travis Goodspeed <travis@radiantmachines.com>
2023-07-22 16:07:05 -0400

    Adding gatorom to macOS installation. #42

--- buildrelease-macos.sh
+++ buildrelease-macos.sh
@@ -11,9 +11,9 @@ set -e
 ## These paths are hardcoded to prevent Homebrew for gunking things
 ## up.  Be sure to install *everything* in that version of Qt, or
 ## you'll be missing important libraries like QtCharts.
-export QTDIR=~/Qt/6.5.1/macos/bin
-export CMAKE=~/Qt/6.5.1/macos/bin/qt-cmake
-export DEPLOYQT=~/Qt/6.5.1/macos/bin/macdeployqt
+export QTDIR=~/Qt/6.5.2/macos/bin
+export CMAKE=~/Qt/6.5.2/macos/bin/qt-cmake
+export DEPLOYQT=~/Qt/6.5.2/macos/bin/macdeployqt
 
 
 
@@ -24,13 +24,13 @@ mkdir build
 (cd build && $CMAKE .. && make -j 8)
 
 mkdir release
-cp -rf build/maskromtool.app release/
+cp -rf build/maskromtool.app build/gatorom release/
 # Hardcoding the executable is bad, but the Homebrew version will break the build.
 $DEPLOYQT release/maskromtool.app -sign-for-notarization="Developer ID Application: Travis Goodspeed"
 # Verify the signature
 codesign --verify --verbose  release/maskromtool.app
 
 # Zip up a release.
-(cd release && zip -r maskromtool-macos-`uname -m`.zip maskromtool.app)
+(cd release && zip -r maskromtool-macos-`uname -m`.zip maskromtool.app gatorom)
 
 
